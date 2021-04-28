5108b711559534d803ad90132f3f043720e30aa9
孙家奇 <3023493319@qq.com>
2021-04-28 13:52:11 +0800

    '提交'

--- handler/handler.go
+++ handler/handler.go
@@ -1,11 +1,15 @@
 package handler
 
 import (
+	"encoding/json"
+	"filestore-server/meta"
+	"filestore-server/util"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
 func UploadHandler(w http.ResponseWriter,r *http.Request){
@@ -24,21 +28,47 @@ func UploadHandler(w http.ResponseWriter,r *http.Request){
 				return
 			}
             defer file.Close()
-            newFile,err:=os.Create("/tmp/"+head.Filename)
+            fileMeta:=meta.FileMeta{
+            	FileName: head.Filename,
+            	Location:"./upload/"+head.Filename,
+            	UploadAt: time.Now().Format("2021-06-02 15:04:05"),
+
+			}
+            newFile,err:=os.Create(fileMeta.Location)
             if err!=nil{
             	fmt.Printf("Failed to create file,err%s\n",err.Error())
             	return
 			}
 			defer newFile.Close()
-            _,err=io.Copy(newFile,file)
+            fileMeta.FileSize,err=io.Copy(newFile,file)
             if err!=nil{
             	fmt.Printf("Failed to save data into file,err:%s\n",err.Error())
             	return
 			}
+			newFile.Seek(0,0)
+            fileMeta.FileSha1=util.FileSha1(newFile)
+            meta.UpdateFileMeta(fileMeta)
 			http.Redirect(w,r,"/file/upload/suc",http.StatusFound)
 		}
 }
 //上传已完成
 func UploadSucHandler(w http.ResponseWriter,r *http.Request){
 	io.WriteString(w,"Upload finished")
+}
+// 获取文件元信息
+func GetFileMetaHandler(w http.ResponseWriter,r *http.Request){
+	r.ParseForm()
+	filehash:=r.Form["filehash"][0]
+	fMeta:=meta.GetFileMeta(filehash);
+	data,err:=json.Marshal(fMeta)
+	if err!=nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+}
+
+//下载信息
+func DownloadHandler(w http.ResponseWriter,r *http.Request)  {
+	
 }
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main(){
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/file/upload",handler.UploadHandler)
 	http.HandleFunc("/file/upload/suc",handler.UploadSucHandler)
+	http.HandleFunc("/file/meta",handler.GetFileMetaHandler)
 
 	err:=http.ListenAndServe(":8080",nil)
 	if err!=nil{
