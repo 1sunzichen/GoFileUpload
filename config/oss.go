8a19dbc0e721ba881c447eb6f2fd515e3f004a0c
romantic艾克 <3023493319@qq.com>
2021-05-25 10:43:46 +0800

    Update oss.go

--- config/oss.go
+++ config/oss.go
@@ -2,7 +2,7 @@ package config
 const (
 	OSSBucket="ekkofile-store"
 	OSSEndpoint="oss-cn-beijing.aliyuncs.com"
-	OSSAccessKeyID="LTAI5tA8amT2P5TiYB73XTos"
-	OSSAccessKeySecret="W278E8lwasu1IrK0iZSKPlTKAnuHTe"
+	OSSAccessKeyID=""
+	OSSAccessKeySecret=""
 
 )
